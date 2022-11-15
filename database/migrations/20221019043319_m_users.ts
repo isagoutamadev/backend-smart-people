a1a510eee6a9c0c69c7bfd3b31a967ea8a79b017
Isa Goutama <isagoutama.dev@gmail.com>
2022-11-15 11:38:15 +0700

    remove unncessary data

--- database/migrations/20221019043319_m_users.ts
+++ database/migrations/20221019043319_m_users.ts
@@ -2,23 +2,22 @@ import { Knex } from "knex";
 
 
 export async function up(knex: Knex): Promise<void> {
-  return knex.schema.createTable("m_users", function (table) {
-    table.uuid("id").primary().notNullable();
-    table.string("email").unique().notNullable();
-    table.string("password").notNullable();
-    table.boolean("is_active").notNullable();
-    table.boolean("is_banned").notNullable();
-    table.timestamp("last_login").nullable();
-    table.uuid("employee_id").unique().references("m_employees.id").notNullable();
-    table.integer("role_id").unsigned().references("lt_roles.id").notNullable();
-    table.string("device_id").nullable();
-    table.string("fcm_token").nullable();
-    table.uuid("created_by").nullable();
-    table.uuid("updated_by").nullable();
-    table.timestamps();
-  });
+  // return knex.schema.createTable("m_users", function (table) {
+  //   table.uuid("id").primary().notNullable();
+  //   table.string("email").unique().notNullable();
+  //   table.string("password").notNullable();
+  //   table.boolean("is_active").notNullable();
+  //   table.boolean("is_banned").notNullable();
+  //   table.timestamp("last_login").nullable();
+  //   table.integer("role_id").unsigned().references("lt_roles.id").notNullable();
+  //   table.string("device_id").nullable();
+  //   table.string("fcm_token").nullable();
+  //   table.uuid("created_by").nullable();
+  //   table.uuid("updated_by").nullable();
+  //   table.timestamps();
+  // });
 }
 
 export async function down(knex: Knex): Promise<void> {
-  return knex.schema.dropTable("m_users");
+  // return knex.schema.dropTable("m_users");
 }
