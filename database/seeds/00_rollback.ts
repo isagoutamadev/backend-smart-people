248344a0415e72d85124ebed54fed997847cc9e4
lela <isagoutama.dev@gmail.com>
2022-11-24 14:06:33 +0700

    reservations seeder

--- database/seeds/00_rollback.ts
+++ database/seeds/00_rollback.ts
@@ -3,7 +3,7 @@ import { Knex } from "knex";
 export async function seed(knex: Knex): Promise<void> {
     // Deletes ALL existing entries
     await knex("m_users").del();
-    // await knex("m_employees").del();
+    await knex("m_reservations").del();
     // await knex("lt_roles").del();
     // await knex("m_branch_companies").del();
     // await knex("lt_districts").del();
