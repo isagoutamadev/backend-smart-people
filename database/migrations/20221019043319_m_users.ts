53640fc9bfa73c63dd5b2237f513e776ee140aa2
lela <isagoutama.dev@gmail.com>
2022-12-19 11:53:29 +0700

    m_users name, created_by, updated_by, deleted_by

--- database/migrations/20221019043319_m_users.ts
+++ database/migrations/20221019043319_m_users.ts
@@ -6,9 +6,13 @@ export async function up(knex: Knex): Promise<void> {
     table.increments("id").primary().notNullable();
     table.uuid("uuid").unique().notNullable();
     table.integer("role_id").unsigned().notNullable().references("lt_roles.id");
+    table.string("name", 50).notNullable();
     table.string("username", 50).unique().notNullable();
     table.string("email", 50).unique().notNullable();
     table.string("password").notNullable();
+    table.integer("created_by").unsigned().notNullable().references("m_users.id");
+    table.integer("updated_by").unsigned().nullable().references("m_users.id");
+    table.integer("deleted_by").unsigned().nullable().references("m_users.id");
     table.timestamps();
     table.timestamp("deleted_at").nullable();
   });
