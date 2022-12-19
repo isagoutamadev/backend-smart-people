c3f3e07a71519135034d4be491da7eb7d9819eb5
lela <isagoutama.dev@gmail.com>
2022-12-19 11:54:41 +0700

    new users seeder

--- database/seeds/06_users.ts
+++ database/seeds/06_users.ts
@@ -3,33 +3,45 @@ import AuthHelper from "../../src/helpers/auth.helper";
 
 export async function seed(knex: Knex): Promise<void> {
     await knex("m_users").insert([
-        { 
-            uuid: "ab924e94-3f5e-49c9-be02-5896f1efc25b", 
+        {
+            id: 1,
+            uuid: "ab924e94-3f5e-49c9-be02-5896f1efc25b",
+            name: "Super Admin",
             email: "admin@konekthing.com",
             role_id: 1,
             username: "admin", 
             password: AuthHelper.encrypt("admin"),
+            created_by: 1,
         },
-        { 
-            uuid: "bc924e94-3f5e-49c9-be02-5896f1efc25b", 
+        {
+            id: 2,
+            uuid: "bc924e94-3f5e-49c9-be02-5896f1efc25b",
+            name: "PIC Jakarta",
             email: "jakarta@konekthing.com",
             role_id: 3,
             username: "jakarta", 
             password: AuthHelper.encrypt("jakarta"),
+            created_by: 1,
         },
-        { 
+        {
+            id: 3,
             uuid: "cd924e94-3f5e-49c9-be02-5896f1efc25b", 
+            name: "PIC Jaktim",
             email: "jaktim@konekthing.com",
             role_id: 4,
             username: "jaktim", 
             password: AuthHelper.encrypt("jaktim"),
+            created_by: 1,
         },
-        { 
-            uuid: "de924e94-3f5e-49c9-be02-5896f1efc25b", 
+        {
+            id: 4,
+            uuid: "de924e94-3f5e-49c9-be02-5896f1efc25b",
+            name: "PIC Jatinegara",
             email: "jatinegara@konekthing.com",
             role_id: 5,
             username: "jatinegara", 
             password: AuthHelper.encrypt("jatinegara"),
+            created_by: 1,
         },
     ]);
 };
